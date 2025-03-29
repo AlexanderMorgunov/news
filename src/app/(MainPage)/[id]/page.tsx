4bf759952cf8fe057bc03aade1d60b672d340720
Alex <morgunowaleks@uandex.ru>
2025-03-30 00:44:22 +0300

    fix(page): fix error

--- src/app/(MainPage)/[id]/page.tsx
+++ src/app/(MainPage)/[id]/page.tsx
@@ -2,5 +2,5 @@ import { DetailNewsPage } from "@/fsd/pages/DetailNewsPage/DetailNewsPage";
 
 export default async function Page({ params }: { params: { id: number } }) {
   const { id } = await params;
-  return <DetailNewsPage id={id} />;
+  return <DetailNewsPage id={Number(id)} />;
 }
